7bc0874386f17b67e8238f351a50b400c8994d07
Guilherme d'Almeida <guilhermebarros181@gmail.com>
2025-10-17 15:54:45 -0300

    desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone celularEsposa = new(numero: "9999-8888", modelo: "Iphone X", imei: "123456789012345", memoria: 64);
+Nokia celularMarido = new(numero: "7777-6666", modelo: "Nokia 8", imei: "987654321098765", memoria: 128);
+
+Console.WriteLine("Celular da Esposa:");
+celularEsposa.Ligar();
+celularEsposa.ReceberLigacao();
+celularEsposa.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Celular do Marido:");
+celularMarido.Ligar();
+celularMarido.ReceberLigacao();
+celularMarido.InstalarAplicativo("WhatsApp");
